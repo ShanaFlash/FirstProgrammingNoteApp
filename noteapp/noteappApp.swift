35ed6992be5b5bb876b2d75c75e51503a69bb84b
ShanaFlash <nancy17flash@gmail.com>
2025-09-07 17:55:19 -0400

    Initial Commit

---
+++ noteapp/noteappApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteappApp.swift
+//  noteapp
+//
+//  Created by ShanaWen on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct noteappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
